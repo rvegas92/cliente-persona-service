1904af3796de7a927100d6602ad5437f0443dfd3
rvegas92 <rvega@nisira.com.pe>
2024-07-20 07:13:47 -0500

    feat: inicio del proyecto

---
+++ src/test/java/com/nttdata/pruebatecnica/cliente_persona_service/ClientePersonaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.pruebatecnica.cliente_persona_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePersonaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
